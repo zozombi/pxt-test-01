76f704a7b344568b919170fcdb3db2894665fb52
zozombi <37424297+zozombi@users.noreply.github.com>
2020-01-01 17:21:44 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
+//% color=190 weight=100 icon="\uf1ec" block="Zozo Blocks"
+namespace zozo {
+    led.plot(0, 0)
+}
 
--- test.ts
+++ test.ts
@@ -1,4 +1,1 @@
 // tests go here; this will not be compiled when this package is used as a library
-
-led.point(0, 0)
-led.toggle(0, 0)
