e2b9967580992ec755ee4d17fb698cabd83a11b6
zozombi <37424297+zozombi@users.noreply.github.com>
2020-01-01 17:23:26 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 //% color=190 weight=100 icon="\uf1ec" block="Zozo Blocks"
 namespace zozo {
-    led.plot(0, 0)
+
+    //% block
+    export function LEDplot() {
+        led.plot(0, 0)
+    }
+
 }
 
