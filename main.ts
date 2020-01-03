0cfa38f16511b47a6f4be40693b7824214097fb9
zozombi <37424297+zozombi@users.noreply.github.com>
2020-01-03 22:49:57 +0100

    test3

--- main.ts
+++ main.ts
@@ -1,14 +1,10 @@
-//% color=190 weight=100 icon="\uf1ec" block="Zozo Blocks 2"
-namespace zozo {
-    /**
-     * Turns all LEDS on
-    */
-    //% help=led/plot-all
-    //% parts="ledmatrix"
-    export function plotAll(): void {
-        for (let i = 0; i < 5; i++) {
-            for (let j = 0; j < 5; j++) {
-
-        }
-    }
-}
+basic.plotLeds(`
+    # # . # #
+    . . # . .
+    . . # . .
+    # . . . #
+    . # # # .
+    `);
+basic.pause(300);
+basic.showString("Hello");
+// foo.bar();
