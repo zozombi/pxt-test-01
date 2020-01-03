9f43bc05a01e123a8431ebbdfb73d6aa0d88b06a
zozombi <37424297+zozombi@users.noreply.github.com>
2020-01-03 22:58:57 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
-basic.plotLeds(`
-    # # . # #
-    . . # . .
-    . . # . .
-    # . . . #
-    . # # # .
-    `);
-basic.pause(300);
-basic.showString("Hello");
-// foo.bar();
+/**
+ * Provides access to basic micro:bit functionality.
+ */
+//% color=190 weight=100 icon="\uf1ec" block="ZozoBlocks"
+namespace zozoblocks {
+    basic.plotLeds(`
+        # # . # #
+        . . # . .
+        . . # . .
+        # . . . #
+        . # # # .
+        `);
+    basic.pause(300);
+    basic.showString("Hello");
+    // foo.bar();
+}
